0317413ac64bff405ab57ea2901f717f46e6e998
SergeyVolkoff <vol7745@yandex.ru>
2024-02-13 06:32:46 -0500

    test_feature_review

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
