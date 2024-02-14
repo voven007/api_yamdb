0fbe1b8e10004f0213567c72dc439076d8c7a349
Владимир Бабенко <babenko-nn@yandex.ru>
2024-02-14 20:13:31 +0300

    Castom user.model 2

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
+from .models import MyUser
 
-# from .models import MyUser
+
+@admin.register(MyUser)
+class MyUserAdmin(admin.ModelAdmin):
+    pass
 
 # UserAdmin.fieldsets += (
 #     ('Extra Fields', {'fields': (
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django import forms
+# from django import forms
 
 
 # ENUM_CHOICES = [
