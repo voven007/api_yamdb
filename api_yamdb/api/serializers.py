ed6258ce88edfe5de439948c6e73fb88428fa3aa
SergeyVolkoff <vol7745@yandex.ru>
2024-02-18 12:33:53 -0500

    edit serial

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,8 @@
+from django.forms import ValidationError
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 
-from reviews.models import Comment, Review
+from reviews.models import Comment, Review, Title
 
 
 
@@ -12,10 +14,21 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ('__all__')
+    
+    def validate(self, data):
+        request = self.context['request']
+        title_id = self.context['view'].kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (request.method == 'POST' and Review.objects.filter(
+                author=request.user, title=title).exists()):
+            raise ValidationError(
+                'Нельзя сделать более одного отзыва!'
+            )
+        return data
 
 class CommentSerializer(serializers.ModelSerializer):
      author = serializers.StringRelatedField(
-        read_only=True
+        read_only=True,
     )
 
      class Comment:
